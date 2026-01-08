9e32cd72358d516527fc0d2f722ae6b16f2bb00e
RIAN <kang.y2824@gmail.com>
2026-01-08 17:21:02 +0900

    기초 다지기
    C# 이랑 전혀안맞는 사람이 열심히 배웠(?) 습니다. 까먹는걸 밥먹듯이하지만 여기에 적어두면 알아서 기억하지 않을까요.

---
+++ 첫날깨짐.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anananaanananananan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i; // 정수형 (음수,0 ,자연수)
+            //float f; // 실수형 (소수점) 
+            //double a; // 범위가 넓은 실수형
+            //char c; // 문자 한개 
+            //string d; // 문자 하나도 ok 단어 문장 ok
+
+            //float i = 5.23245234211234f; 
+            //double d = 5.23245234211234f;
+            //Console.WriteLine("i 의 값은 {0}이고 d의 값은 {1} 이다.",i,d);
+
+            //Write = 입력 (밑에거 줄바꿈 x) writeLine = 출력 (줄바꿈 o) Wn = 뭐든 엔터로 내려감 
+            //Console.WriteLine("?????"); 
+            //Console.WriteLine("어머");
+
+            //int q = 19;
+            //double w = 2.234546756234f;
+            //char e = '아';
+            //string r = "흐흐히히히히히히힣";
+
+            //Console.Write("숫자, {0}, 실수, {1}, 문자 {2}, 문자열, {3}", q, w, e, r);
+
+
+
+
+            //int number; !!!!
+
+
+            //number = int.Parse(Console.ReadLine());
+            // 넘버에 int로 값을 변환하는데, .Parse 가 바꿔주는 매개체이고 console."ReadLine"이 잘 출력됭 수 있도록 괄호채까지 통채로 끼워넣는다. ......
+            // 무슨 값을 입력하던 정수로 받아드리고 그대로 출력한다 
+            // 문자열은 쓰지 않아도 됨 
+            //Console.WriteLine("number, {0}", number);
+
+
+
+
+
+            //Console.WriteLine ("__"); 를 붙여넣는것은 출력 값이 1 이상이기 떄문에 오류가 나 적어주는것.
+            //string q;
+            //int w;
+            
+            //Console.WriteLine("아 겁나어렵네 C.va");
+            //q = Console.ReadLine();
+            //Console.WriteLine("숫자를 입력해주세요");
+            //w = int.Parse(Console.ReadLine());
+            //Console.WriteLine("문자, {0}, 숫자, {1}", q, w);
+
+            //너무어려운데요 특히 위에꺼 
+
+            //double a;
+            //string b;
+            //string d;
+            //char c;
+            //char e;
+
+            //Console.WriteLine("힘든만큼 소수점을 입력해주세요");
+            //a = double.Parse(Console.ReadLine());
+            //Console.WriteLine("대체이게뭐에요");
+            //b = Console.ReadLine();
+            //Console.WriteLine("C언어가 밉다면 네 라고 대답해주세요");
+            //c = Console.ReadLine()[0];
+            //Console.WriteLine("귀엽다고 잘하는데 라는 칭찬에 말에 기분이 좋아졌다면 네 라고 대답해주세요");
+            //d = Console.ReadLine();
+            //Console.WriteLine("외마디 비명을 질러주세요");
+            //e = Console.ReadLine()[0];
+
+            //Console.WriteLine("{0}, {1}, {2}, {3}, {4}",a, b, c, d, e);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
